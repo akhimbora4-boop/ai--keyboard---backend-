0d013467ebf7cb4837c43e494e97145080721ec1
akhimbora4-boop <akhimbora4@gmail.com>
2026-08-12 22:16:28 +0530

    Update KeyboardService.kt

--- app/src/main/java/com/asim/keyboard/KeyboardService.kt
+++ app/src/main/java/com/asim/keyboard/KeyboardService.kt
@@ -3,65 +3,63 @@ package com.asim.keyboard
 import android.inputmethodservice.InputMethodService
 import android.inputmethodservice.Keyboard
 import android.inputmethodservice.KeyboardView
-import android.media.AudioManager
+import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputConnection
 
 class KeyboardService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
 
     private lateinit var keyboardView: KeyboardView
-    private lateinit var keyboard: Keyboard
-    private var caps = false
+    private lateinit var keyboard: Keyboard // QWERTY
+    private lateinit var symbolsKeyboard: Keyboard // 123
+    private var isSymbols = false
 
     override fun onCreateInputView(): View {
         keyboardView = layoutInflater.inflate(R.layout.keyboard_view, null) as KeyboardView
+
         keyboard = Keyboard(this, R.xml.keyboard)
+        symbolsKeyboard = Keyboard(this, R.xml.symbols)
+
         keyboardView.keyboard = keyboard
         keyboardView.setOnKeyboardActionListener(this)
         return keyboardView
     }
 
     override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
-        val inputConnection: InputConnection = currentInputConnection
-        playClick(primaryCode)
+        val ic: InputConnection? = currentInputConnection
+        if (ic == null) return
 
         when (primaryCode) {
             -1 -> { // SHIFT
-                caps = !caps
-                keyboard.isShifted = caps
-                keyboardView.invalidateAllKeys()
             }
-            -5 -> { // BACKSPACE
-                inputConnection.deleteSurroundingText(1, 0)
+            -2 -> { // 123 / ABC button
+                isSymbols = !isSymbols
+                if (isSymbols) {
+                    keyboardView.keyboard = symbolsKeyboard
+                } else {
+                    keyboardView.keyboard = keyboard
+                }
             }
-            -2 -> { // SYMBOLS - ইয়াত আন keyboard layout load কৰিব পাৰি
-                // TODO: symbols keyboard
+            -3 -> { // Emoji
             }
-            32 -> { // SPACE
-                inputConnection.commitText(" ", 1)
+            -4 -> { // =/< button
+            }
+            -5 -> { // DELETE
+                ic.deleteSurroundingText(1, 0)
             }
             10 -> { // ENTER
-                inputConnection.sendKeyEvent(android.view.KeyEvent(android.view.KeyEvent.ACTION_DOWN, android.view.KeyEvent.KEYCODE_ENTER))
+                ic.sendKeyEvent(KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER))
             }
-            else -> {
-                var code = primaryCode
-                if (caps) code = Character.toUpperCase(code)
-                inputConnection.commitText(code.toChar().toString(), 1)
+            32 -> { // SPACE
+                ic.commitText(" ", 1)
+            }
+            else -> { // Normal letter/number
+                val code = primaryCode.toChar()
+                ic.commitText(code.toString(), 1)
             }
         }
     }
 
-    private fun playClick(keyCode: Int) {
-        val am = getSystemService(AUDIO_SERVICE) as AudioManager
-        when (keyCode) {
-            32 -> am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR)
-            10 -> am.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN)
-            -5 -> am.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE)
-            else -> am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD)
-        }
-    }
-
-    // বাকী function বোৰ খালী ৰাখা
     override fun onPress(primaryCode: Int) {}
     override fun onRelease(primaryCode: Int) {}
     override fun onText(text: CharSequence?) {}
